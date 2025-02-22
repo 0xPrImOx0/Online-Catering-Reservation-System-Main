cecfe47dd20b2a335fa41b379e722f7f8553f5fc
Rey Daug <daug.rey34@gmail.com>
2025-02-22 19:16:29 +0800

    ✅ Add login form validation, theme provider, initial responsive layout for login, and UI enhancements

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -25,13 +25,71 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { useForm } from "react-hook-form";
 import Link from "next/link";
 
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { loginSchema } from "@/utils/formValidtion";
+import { toast } from "sonner";
+
 export default function Home() {
-  const form = useForm();
+  const { setTheme } = useTheme();
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "", // Ensuring controlled input
+      password: "",
+    },
+    mode: "onChange", // Immediate validation feedback
+  });
+
+  const onSubmit = (values: z.infer<typeof loginSchema>) => {
+    try {
+      console.log(values);
+      toast(
+        <div className="p-4">
+          <p>{JSON.stringify(values, null, 2)}</p>
+        </div>
+      );
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  };
   return (
-    <div className={`flex flex-row h-screen`}>
-      <div className="flex flex-1 justify-center items-center ">
-        <Card className="w-[50%] px-3 py-6">
-          <CardHeader className="flex items-center">
+    <div className={`flex flex-col lg:flex-row h-screen overflow-auto`}>
+      <div className="flex flex-1 order-2 lg:order-1 justify-center items-center p-10">
+        <div className="absolute top-5 left-5">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <Card className="w-[90%] md:w-[80%] lg:w-[75%] xl:w-[70%] 2xl:w-[50%] px-3 py-6">
+          <CardHeader className="flex items-center text-center">
             <CardTitle>Login to your account</CardTitle>
             <CardDescription>
               Enter your email below to login to your account
@@ -39,7 +97,10 @@ export default function Home() {
           </CardHeader>
           <CardContent>
             <Form {...form}>
-              <form className="space-y-5">
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-5"
+              >
                 <div className="space-y-4 mb-8">
                   <FormField
                     control={form.control}
@@ -48,7 +109,11 @@ export default function Home() {
                       <FormItem>
                         <FormLabel>Email</FormLabel>
                         <FormControl>
-                          <Input placeholder="Email Address" {...field} />
+                          <Input
+                            placeholder="Email Address"
+                            type="email"
+                            {...field}
+                          />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
@@ -59,12 +124,7 @@ export default function Home() {
                     name="password"
                     render={({ field }) => (
                       <FormItem>
-                        <div className="flex items-center text-sm justify-between">
-                          <FormLabel>Password</FormLabel>
-                          <Button className="text-slate-500" variant="link">
-                            <Link href="">Forgot your password?</Link>
-                          </Button>
-                        </div>
+                        <FormLabel>Password</FormLabel>
                         <FormControl>
                           <Input placeholder="Password" {...field} />
                         </FormControl>
@@ -79,27 +139,27 @@ export default function Home() {
                   </Button>
                   <div className="flex w-full items-center justify-center mb-3">
                     <div className="flex-1 border-t-2 border-slate-300"></div>
-                    <p className="flex-1 py-4 px-2">or continue with</p>
+                    <p className="flex-1 whitespace-nowrap py-4 px-2 text-center">
+                      or continue with
+                    </p>
                     <div className="flex-1 border-t-2 border-slate-300"></div>
                   </div>
-                  <Button className="w-full py-5" variant="outline">
-                    Login with Facebook
-                  </Button>
                 </div>
               </form>
+              <Button className="w-full py-5" variant="outline" asChild>
+                <Link href="/">Login with Facebook</Link>
+              </Button>
             </Form>
           </CardContent>
-          <CardFooter className="flex justify-center">
-            <p>
-              Don't Have an Account?
-              <Button className="px-2 text-base" variant="link" asChild>
-                <Link href="">Sign Up</Link>
-              </Button>
-            </p>
+          <CardFooter className="flex flex-row justify-center">
+            <p>Don't Have an Account?</p>
+            <Button className="px-2 responsive-text" variant="link" asChild>
+              <Link href="/">Sign Up</Link>
+            </Button>
           </CardFooter>
         </Card>
       </div>
-      <div className="flex flex-1 justify-center items-center border border-green-500">
+      <div className="flex flex-1 justify-center items-center order-1 lg:order-2 min-h-[500px] border border-green-500">
         <Skeleton className="h-[80%] w-[80%] rounded-xl" />
       </div>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 export const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        <Toaster position="top-right" richColors />
+      </body>
     </html>
   );
 }
