33978b670434f34065ca252db46757c0facd8f16
Rey Daug <daug.rey34@gmail.com>
2025-02-23 23:28:20 +0800

    ✅ Add ThemeMode component for theme toggling in the authentication page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -25,18 +25,12 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { useForm } from "react-hook-form";
 import Link from "next/link";
 
-import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { loginSchema } from "@/utils/formValidtion";
 import { toast } from "sonner";
+import ThemeMode from "@/components/ThemeMode";
 
 export default function Home() {
   const { setTheme } = useTheme();
@@ -67,26 +61,7 @@ export default function Home() {
     <div className={`flex flex-col lg:flex-row h-screen overflow-auto`}>
       <div className="flex flex-1 order-2 lg:order-1 justify-center items-center p-10">
         <div className="absolute top-5 left-5">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="icon">
-                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span className="sr-only">Toggle theme</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem onClick={() => setTheme("light")}>
-                Light
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("dark")}>
-                Dark
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("system")}>
-                System
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <ThemeMode />
         </div>
         <Card className="w-[90%] md:w-[80%] lg:w-[75%] xl:w-[70%] 2xl:w-[50%] px-3 py-6">
           <CardHeader className="flex items-center text-center">
