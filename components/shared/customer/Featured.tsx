7d263411c3abe5a620307b24de038f7bdfea2839
Rey Daug <daug.rey34@gmail.com>
2025-04-15 20:24:25 +0800

    refactor: 🛠️  Improve featured packages fetching logic and enhance component structure

--- components/shared/customer/Featured.tsx
+++ components/shared/customer/Featured.tsx
@@ -1,36 +1,54 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 import CustomerPackageCard from "./CustomerPackageCard";
 import { CateringPackagesProps } from "@/types/package-types";
 import api from "@/lib/axiosInstance";
+import { useEffect, useState } from "react";
+// use this if you want to render the packages offline
 // import { cateringPackages } from "@/lib/customer/packages-metadata";
 
-async function fetchFeatured() {
-  const response = await api.get("/packages/featured");
-  return response.data.data;
-}
+export default function Featured() {
+  const [cateringPackages, setCateringPackages] = useState<
+    Array<CateringPackagesProps>
+  >([]);
 
-export default async function Featured() {
-  const cateringPackages = await fetchFeatured();
+  useEffect(() => {
+    const fetchFeatured = async () => {
+      const response = await api.get("/packages/featured");
+      setCateringPackages(response.data.data);
+    };
+
+    fetchFeatured();
+  }, []);
   return (
     <section className="px-[5%] mt-24 flex flex-col items-center gap-14">
       <div className="">
-        <h2 className="text-3xl font-bold mb-2 text-center">
+        <h2 className="text-4xl font-bold mb-2 text-center">
           Featured Packages
         </h2>
-        <p className="text-muted-foreground text-center max-w-2xl mx-auto mb-12">
+        <p className="text-muted-foreground text-center mb-12 text-lg">
           Select from our most popular catering packages for weddings, corporate
           events, and private parties.
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {/* Package Showcase */}
-          {cateringPackages
-            .slice(0, 6)
-            .map((pkg: CateringPackagesProps, index: number) => (
-              <CustomerPackageCard item={pkg} key={index} />
-            ))}
+          {cateringPackages.length > 0 ? (
+            cateringPackages
+              .slice(0, 6)
+              .map((pkg: CateringPackagesProps, index: number) => (
+                <CustomerPackageCard item={pkg} key={index} />
+              ))
+          ) : (
+            <div className="col-span-3 min-h-[50vh] flex justify-center items-center text-center">
+              <span className="font-bold text-3xl">
+                No Featured Packages Found
+              </span>{" "}
+            </div>
+          )}
         </div>
       </div>
       <Button size={"landing"} variant={"outline"} asChild>
