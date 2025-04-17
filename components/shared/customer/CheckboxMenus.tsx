429212ac4a2c5a0e1b7c0c90d46b0d085e2ff1cc
Samm Caagbay <sammcaag@gmail.com>
2025-04-17 17:58:33 +0800

    Add: Implement image dialog for menu items in CheckboxMenus component

--- components/shared/customer/CheckboxMenus.tsx
+++ components/shared/customer/CheckboxMenus.tsx
@@ -1,10 +1,19 @@
+"use client";
 import { Checkbox } from "@/components/ui/checkbox";
 import { FormControl, FormLabel } from "@/components/ui/form";
 import { Label } from "@/components/ui/label";
 import { useReservationForm } from "@/hooks/use-reservation-form";
 import { menuItems } from "@/lib/menu-lists";
 import { CategoryProps, MenuItem } from "@/types/menu-types";
-import React from "react";
+import React, { useState } from "react";
+import ImageDialog from "../ImageDialog";
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function CheckboxMenus({
   category,
@@ -22,43 +31,70 @@ export default function CheckboxMenus({
   const getMenusByCategory = (category: CategoryProps) => {
     return menuItems.filter((menu: MenuItem) => menu.category === category);
   };
+
+  const [isImageDialogOpen, setIsImageDialogOpen] = useState(false);
+  const [activeMenu, setActiveMenu] = useState("");
   return (
-    <div key={category} className="space-y-2">
-      <FormLabel className="font-medium text-base">
-        {category} Options
-      </FormLabel>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-        {getMenusByCategory(category).map((menu) => (
-          <div key={menu._id} className="flex items-start space-x-2">
-            <FormControl>
-              <Checkbox
-                id={menu._id}
-                checked={field.value[category]?.includes(menu._id)}
-                disabled={
-                  !field.value[category]?.includes(menu._id) && // Allow unchecking
-                  field.value[category]?.length >= count // Disable if limit is reached
-                }
-                onCheckedChange={(checked) =>
-                  handleCheckboxChange(checked, field, category, menu, count)
-                }
-              />
-            </FormControl>
-            <div className="grid gap-1.5">
-              <Label htmlFor={menu._id} className="font-medium">
-                {menu.name}
-              </Label>
-              <p className="text-sm text-muted-foreground">
-                {menu.shortDescription}
-              </p>
+    <>
+      <div key={category} className="space-y-2">
+        <FormLabel className="font-medium text-base">
+          {category} Options
+        </FormLabel>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+          {getMenusByCategory(category).map((menu) => (
+            <div key={menu._id} className="flex items-start space-x-2">
+              <FormControl>
+                <Checkbox
+                  id={menu._id}
+                  checked={field.value[category]?.includes(menu._id)}
+                  disabled={
+                    !field.value[category]?.includes(menu._id) && // Allow unchecking
+                    field.value[category]?.length >= count // Disable if limit is reached
+                  }
+                  onCheckedChange={(checked) =>
+                    handleCheckboxChange(checked, field, category, menu, count)
+                  }
+                />
+              </FormControl>
+              <div className="flex flex-col justify-between">
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button
+                        size={"custom"}
+                        variant={"link"}
+                        className="font-medium max-w-fit -mt-1"
+                        onClick={() => {
+                          setActiveMenu(menu._id);
+                          setIsImageDialogOpen(true);
+                        }}
+                      >
+                        {menu.name}
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>View Image</TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+                <p className="text-sm text-muted-foreground">
+                  {menu.shortDescription}
+                </p>
+                {isImageDialogOpen && activeMenu === menu._id && (
+                  <ImageDialog
+                    item={menu}
+                    isImageDialogOpen={isImageDialogOpen}
+                    setIsImageDialogOpen={setIsImageDialogOpen}
+                  />
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        {selectedMenus[category]?.length >= count && (
+          <p className="text-xs text-muted-foreground italic">
+            *You can only select up to {count} item/s for {category}.*
+          </p>
+        )}
       </div>
-      {selectedMenus[category]?.length >= count && (
-        <p className="text-xs text-muted-foreground italic">
-          *You can only select up to {count} item/s for {category}.*
-        </p>
-      )}
-    </div>
+    </>
   );
 }
