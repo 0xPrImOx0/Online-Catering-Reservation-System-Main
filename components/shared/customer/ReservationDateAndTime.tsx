fd8b2163838e269f89135d769ddccc84aa7ef9a3
Samm Caagbay <sammcaag@gmail.com>
2025-04-26 15:06:20 +0800

    refactor: 🛠️ Enhance reservation date selection with a popover calendar and improve date formatting for better user experience

--- components/shared/customer/ReservationDateAndTime.tsx
+++ components/shared/customer/ReservationDateAndTime.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   FormControl,
   FormField,
@@ -17,6 +18,19 @@ import { ReservationValues } from "@/hooks/use-reservation-form";
 import { useFormContext } from "react-hook-form";
 import { withMask } from "use-mask-input";
 
+import { useId, useState } from "react";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 export default function EventDate({
   control,
   deliveryOption,
@@ -28,6 +42,7 @@ export default function EventDate({
   const {
     formState: { errors },
   } = useFormContext<ReservationValues>();
+  const id = useId();
   return (
     <div className="flex gap-4 mt-4">
       <FormField
@@ -40,15 +55,41 @@ export default function EventDate({
               <span className="text-destructive">*</span>{" "}
             </FormLabel>
             <FormControl>
-              <Input
-                type="date"
-                {...field}
-                value={
-                  field.value instanceof Date
-                    ? field.value.toISOString().split("T")[0]
-                    : field.value || ""
-                }
-              />
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    id={id}
+                    variant={"outline"}
+                    className={cn(
+                      "group bg-background hover:bg-background border-input w-full justify-between px-3 font-normal outline-offset-0 outline-none focus-visible:outline-[3px]",
+                      !field.value && "text-muted-foreground"
+                    )}
+                  >
+                    <span
+                      className={cn(
+                        "truncate",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value instanceof Date
+                        ? format(field.value, "PPP")
+                        : "Pick a date"}
+                    </span>
+                    <CalendarIcon
+                      size={16}
+                      className="transition-colors text-muted-foreground/80 group-hover:text-foreground shrink-0"
+                      aria-hidden="true"
+                    />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-2" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                  />
+                </PopoverContent>
+              </Popover>
             </FormControl>
             <FormMessage />
           </FormItem>
