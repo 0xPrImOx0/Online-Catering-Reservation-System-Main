42cd058d47a87c691b5c47e5363907dcb1595f7a
Rey Daug <daug.rey34@gmail.com>
2025-04-26 12:06:18 +0800

    refactor: 🛠️ Improve layout and structure of PlatedWarning component for better readability and user interaction

--- components/shared/PlatedWarning.tsx
+++ components/shared/PlatedWarning.tsx
@@ -10,32 +10,33 @@ export default function PlatedWarning() {
   return (
     <div
       className={clsx(
-        "mb-4 p-4 bg-yellow-50 border-2 border-amber-400 rounded-lg flex items-start gap-3 relative",
+        "relative bg-yellow-50 border-2 border-amber-400 rounded-lg flex flex-col gap-3 p-4",
         {
           hidden: closePlatedWarning,
         }
       )}
     >
-      <Button
-        className="absolute right-4 top-4 text-muted-foreground"
-        variant={"ghost"}
-        size={"custom"}
-        onClick={() => setClosePlatedWarning((prev) => !prev)}
-      >
-        <X className="min-w-5 min-h-5" />
-      </Button>
-      <Info className="w-20 sm:w-14 md:w-10 lg:w-6 relative text-yellow-500" />
-      <div className="space-y-2">
-        <h3 className="font-medium dark:text-background">
-          Plated Course Service Fee
-        </h3>
-        <p className="text-sm text-foreground dark:text-background text-justify max-w-[1000px]">
-          Our plated course packages include professional waitstaff who will
-          serve each course directly to your guests&apos; tables. An additional
-          service fee of &#8369;100 per hour is included in the price per
-          person.
-        </p>
+      <div className="flex w-full border border-red-500 relative justify-between items-start ">
+        <div className="flex space-x-2">
+          <Info className="w-6 relative text-yellow-500" />
+          <h3 className="font-medium dark:text-background">
+            Plated Course Service Fee
+          </h3>
+        </div>
+        <Button
+          className="text-muted-foreground"
+          variant={"link"}
+          size={"custom"}
+          onClick={() => setClosePlatedWarning((prev) => !prev)}
+        >
+          <X className="min-w-5 min-h-5" />
+        </Button>
       </div>
+      <p className="text-sm text-foreground dark:text-background text-justify ml-8">
+        Our plated course packages include professional waitstaff who will serve
+        each course directly to your guests&apos; tables. An additional service
+        fee of &#8369;100 per hour is included in the price per person.
+      </p>
     </div>
   );
 }
